a8e9619e22aa9686e26ca2779c3fd978810f9017
Jordan Piscitelli <jordanp@synopsys.com>
2019-05-01 18:04:31 -0400

    Adding all the UI elements. Building out the task.

--- coverityTask/index.ts
+++ coverityTask/index.ts
@@ -7,74 +7,23 @@ var coverityRunner = require("./coverity_runner");
 
 async function run() {
     try {
-        const server: string = tl.getEndpointUrl('coverityService', true);
-
-        const username: string = tl.getEndpointAuthorizationParameter('coverityService', 'username', true);
-        const password: string = tl.getEndpointAuthorizationParameter('coverityService', 'password', true);
         
-        const projectName = tl.getInput('projectName', true);
-        const streamName = tl.getInput('streamName', true);
+        var result = await connect();
 
-        console.log("Starting coverity, connecting to:" + server);
+        if (result){
+            var args = await find_extra_args();
 
-        var connected = await coverityApi.connectAsync(server, username, password);
-        if (!connected || !(coverityApi.client)) {
-            tl.setResult(tl.TaskResult.Failed, 'Could not connect to coverity server.');
-            return;
-        } else {
-            console.log("Connected!");
+            var buildDirectory = tl.getPathInput('cwd', true, false); // tl.getVariable("Agent.BuildDirectory");
+            var idir: string = path.join(buildDirectory, "idir");
+    
+            var commands = await find_commands(result.server, idir, result.streamName);
+            run_commands(result.coverityBin, buildDirectory, commands, args);
+
+            if (result.viewId){
+                check_issues
+                
+            }
         }
-
-        var project = await coverityApi.findProjectAsync(projectName);
-        if (project) {
-            console.log("Found project.");
-        } else {
-            tl.setResult(tl.TaskResult.Failed, 'Given project could not be found on coverity server.');
-            return;
-        }
-
-        fs.writeFileSync("project.json", JSON.stringify(project));
-
-        var stream = await coverityApi.findStreamAsync(project, streamName);
-        if (stream) {
-            console.log("Found stream.");
-        } else {
-            tl.setResult(tl.TaskResult.Failed, 'Given stream could not be found on the given project.');
-            return;
-        }
-
-        console.log("Project: " + project.id.name);
-        console.log("Stream: " + stream.id.name);
-        console.log("Succesfully communicated with coverity server.");
-
-        console.log("Searching for coverity installation.");
-        var bin = coverityInstallation.findCoverityBin();
-        if (bin){
-            console.log("Found coverity bin: " + bin);
-        } else {
-            tl.setResult(tl.TaskResult.Failed, 'Coverity installation could not be found.');
-            return;
-        }
-
-        var buildDirectory = tl.getPathInput('cwd', true, false); // tl.getVariable("Agent.BuildDirectory");
-        var idir: string = path.join(buildDirectory, "idir");
-
-        var cov_build = ["cov-build", "--dir", idir];
-        var cov_analyze = ["cov-analyze", "--dir", idir];
-        var cov_commit = ["cov-commit-defects", "--dir", idir, "--url", server, "--stream", streamName];
-
-        var covBuild = await coverityRunner.runCoverityCommand(bin, buildDirectory, cov_build, []);
-        var covAnalyze = await coverityRunner.runCoverityCommand(bin, buildDirectory, cov_analyze, []);
-        var covCommit = await coverityRunner.runCoverityCommand(bin, buildDirectory, cov_commit, []);
-console.log(covBuild);
-        if (covBuild == 0 && covAnalyze == 0 && covCommit == 0){
-            console.log("OVERALL STATUS: SUCCESS");
-        } else {
-            console.log("OVERALL STATUS: FAILURE");
-
-        }
-
-        
         return;
     }
     catch (err) {
@@ -90,7 +39,126 @@ console.log(covBuild);
     }
 }
 
+interface ConnectResult {
+    server: string,
+    projectKey: string, 
+    streamName: string,
+    viewId?: string
+    coverityBin:string
+}
 
+async function check_issues() {
+    //defects are over http. 
+    //get the views /api/views/v1
+    //find the view id 
+    //find defects for the view /api/viewContents/issues/v1/<viewId>
+    //fail if there are defects
+
+    final int defectSize = getIssueCountForView(projectId, viewId, viewService);
+    logger.info(String.format("[Coverity] Found %s issues for project \"%s\" and view \"%s\"", defectSize, resolvedProjectName, resolvedViewName));
+
+}
+
+async function find_extra_args(){
+    return {
+        "cov-build": tl.getInput("covBuildArgs", false),
+        "cov-analyze": tl.getInput("covAnalyzeArgs", false),
+        "cov-commit-defects": tl.getInput("covCommitArgs", false),
+        "cov-run-desktop": tl.getInput("covDesktopArgs", false),
+    };
+}
+
+async function run_commands(bin:string, buildDirectory:string, commands:Array<Array<string>>, extraArgs:any) {
+    for (var command in commands) {
+        var commandRun = await coverityRunner.runCoverityCommand(bin, buildDirectory, command, extraArgs[command[0]]);
+    }
+}
+
+async function find_commands(server:string, idir:string, streamName:string):Promise<Array<Array<string>>> {
+    var runType = tl.getInput('coverityRunType', true);
+    if (runType == "buildanalyzecommit"){
+        var analysisType = tl.getInput('coverityAnalysisType', true);
+
+        var cov_middle;
+        if (analysisType == "full"){
+            cov_middle = ["cov-analyze", "--dir", idir];
+        }else if (analysisType == "incremental"){
+            cov_middle = ["cov-run-desktop", "--dir", idir, "--url", server, "--stream", streamName];
+        } else {
+            tl.setResult(tl.TaskResult.Failed, 'Unkown coverity run type: ' + runType);
+            return [];
+        }
+
+        var cov_build = ["cov-build", "--dir", idir];
+        var cov_commit = ["cov-commit-defects", "--dir", idir, "--url", server, "--stream", streamName];
+        return [cov_build, cov_middle, cov_commit];
+    } else if (runType == "custom"){
+        return []; //TODO: customCoverityCommands
+    } else {
+        tl.setResult(tl.TaskResult.Failed, 'Unkown coverity run type: ' + runType);
+        return [];
+    }
+}
+
+async function connect(): Promise<ConnectResult|null> {
+    const server: string = tl.getEndpointUrl('coverityService', true);
+
+    const username: string = tl.getEndpointAuthorizationParameter('coverityService', 'username', true);
+    const password: string = tl.getEndpointAuthorizationParameter('coverityService', 'password', true);
+    
+    const projectName = tl.getInput('projectName', true);
+    const streamName = tl.getInput('streamName', true);
+
+    console.log("Starting coverity, connecting to:" + server);
+
+    var connected = await coverityApi.connectAsync(server, username, password);
+    if (!connected || !(coverityApi.client)) {
+        tl.setResult(tl.TaskResult.Failed, 'Could not connect to coverity server.');
+        return null;
+    } else {
+        console.log("Connected!");
+    }
+
+    var project = await coverityApi.findProjectAsync(projectName);
+    if (project) {
+        console.log("Found project.");
+    } else {
+        tl.setResult(tl.TaskResult.Failed, 'Given project could not be found on coverity server.');
+        return null;
+    }
+
+    fs.writeFileSync("project.json", JSON.stringify(project));
+
+    var stream = await coverityApi.findStreamAsync(project, streamName);
+    if (stream) {
+        console.log("Found stream.");
+    } else {
+        tl.setResult(tl.TaskResult.Failed, 'Given stream could not be found on the given project.');
+        return null;
+    }
+
+    console.log("Project: " + project.id.name);
+    console.log("Stream: " + stream.id.name);
+    console.log("Succesfully communicated with coverity server.");
+
+    console.log("Searching for coverity installation.");
+    var bin = coverityInstallation.findCoverityBin();
+    if (bin){
+        console.log("Found coverity bin: " + bin);
+    } else {
+        tl.setResult(tl.TaskResult.Failed, 'Coverity installation could not be found.');
+        return null;
+    }
+
+    var result: ConnectResult = {
+        server: server,
+        projectKey: project.projectKey, 
+        streamName: streamName,
+        coverityBin: bin
+    };
+
+    return result;
+}
 
 run();
 
---
+++ coverityTask/tests/coverity_api_mock.ts
@@ -0,0 +1,77 @@
+import soap = require("soap");
+import CoverityTypes = require("../coverity_types");
+import CoverityApi = require("../coverity_api");
+
+function newMock(shouldConnect: boolean, projectName?:string, streamName?:string) {
+    var stream: CoverityTypes.Stream|null = null;
+    var project: CoverityTypes.Project|null = null;
+    
+    if (projectName){
+        var streams = new Array<CoverityTypes.Stream>();
+        if (streamName){
+            stream = {
+                autoDeleteOnExpiry: false,
+                componentMapId: {
+                    name: "componentMapId"
+                },
+                description: "string",
+                id: {
+                    name: streamName!
+                },
+                language: "string",
+                outdated: true,
+                primaryProjectId: {
+                    name:projectName!
+                },
+                triageStoreId: {
+                    name: "triageId"
+                },
+                enableDesktopAnalysis: true,
+                ownerAssignmentOption: "string"
+            };
+            streams.push(stream);
+        }
+        project = {
+            streams: streams,
+            roleAssignments: new Array<CoverityTypes.RoleAssignment>(),
+            dateCreated: new Date(),
+            dateModified: new Date(),
+            description: "",
+            id: {
+                name: projectName!
+            },
+            projectKey: "",
+            userCreated: "",
+            userModified: ""
+        };
+    }
+
+    var mock:any = {
+        connectAsync: async function (server:string, username:string, password:string): Promise<boolean> {
+            console.log("Mock connect invoked.");
+            if (shouldConnect) {
+                return false;
+            } else {
+                this.client = {};
+                return true;
+            }
+        },
+        findProjectAsync: async function(name:string):Promise<CoverityTypes.Project|null> {
+            console.log("Mock find project invoked.");
+            return project;
+        },
+        findStreamAsync: async function(proect:CoverityTypes.Project, name:string):Promise<CoverityTypes.Stream|null> {
+            console.log("Mock find stream invoked.");            
+            return stream;
+        },
+        createProjectAsync: async function (project:CoverityTypes.ProjectSpec) {
+        },
+        createStreamAsync: async function (projectName: string, stream: CoverityTypes.StreamSpec) {
+        },
+        client: undefined
+    }
+    
+    return mock;
+}
+
+module.exports = newMock;
